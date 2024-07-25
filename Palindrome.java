f4eebc9a3f6eb0fb8cbe5d976a08b45f64e9d1ec
SumathiJawahar <141711453+SumathiGitHub@users.noreply.github.com>
2024-07-25 01:14:39 -0700

    Update Palindrome.java
    added number validation

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,10 @@
 public class Palindrome {
     public static void main(String[] args) {
+        //withString();
+        withNumber();            
+    }
+
+    public static void withString(){
         String name = "Appa";
         //checking cases
         String name1 = name.toUpperCase();
@@ -13,6 +18,22 @@ public class Palindrome {
         }
         else
         System.out.println("The given string is not palindrome!");
-       
+    }
+
+    public static void withNumber(){
+        int number = 12321;
+        int original = number;
+        int reversedNo = 0;
+        while(number>0){
+            int digit = number%10;   // extracting digits
+            reversedNo = reversedNo*10 + digit;
+            number = number/10;   // removing digits
+         }
+         if(original==reversedNo){
+            System.out.println("Palindrome!");
+         }
+         else
+         System.out.println("Not Palindrome!");
+
     }
 }
