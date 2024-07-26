7aa497a2587625694f664e72c34939c96be82cd1
SumathiJawahar <141711453+SumathiGitHub@users.noreply.github.com>
2024-07-26 00:07:17 -0700

    Add files via upload

---
+++ NumberOfWordsinString.java
@@ -0,0 +1,9 @@
+public class NumberOfWordsinString {
+    public static void main(String[] args) {
+        String input = "Hello World Doggy!";
+        String[] noOfWords = input.split(" ");
+        System.out.println("Nunber of words in a String : " + noOfWords.length);
+        
+    }
+    
+}
